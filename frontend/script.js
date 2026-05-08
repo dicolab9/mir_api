ee737afa815eb22268f86da9939eb17e3b5ab03e
dicolab9 <dico.colab9@gmail.com>
2026-05-08 13:33:25 -0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -10,7 +10,11 @@ async function cadastrar() {
     cpf: document.getElementById('cpf').value
   };
 
-  const response = await fetch('http://localhost:3000/cadastro', {
+  //-----------------------------------
+  // FETCH RELATIVO
+  //-----------------------------------
+
+  const response = await fetch('/cadastro', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -33,12 +37,59 @@ async function cadastrar() {
 
 async function estatisticas() {
 
-  const response = await fetch(
-    'http://localhost:3000/estatisticas'
-  );
+  //-----------------------------------
+  // FETCH RELATIVO
+  //-----------------------------------
+
+  const response = await fetch('/estatisticas');
 
   const data = await response.json();
 
   document.getElementById('stats').textContent =
     JSON.stringify(data, null, 2);
 }
+
+// async function cadastrar() {
+
+//   const body = {
+//     nome: document.getElementById('nome').value,
+//     sobrenome: document.getElementById('sobrenome').value,
+//     rua: document.getElementById('rua').value,
+//     casa: document.getElementById('casa').value,
+//     cidade: document.getElementById('cidade').value,
+//     cep: document.getElementById('cep').value,
+//     cpf: document.getElementById('cpf').value
+//   };
+
+//   const response = await fetch('http://localhost:3000/cadastro', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(body)
+//   });
+
+//   const data = await response.json();
+
+//   document.getElementById('resultado').innerHTML = `
+//     <h2>Cadastro Realizado</h2>
+
+//     <h3>Modo Tradicional</h3>
+//     <pre>${JSON.stringify(data.normal, null, 2)}</pre>
+
+//     <h3>MIR + MNE</h3>
+//     <pre>${JSON.stringify(data.mir, null, 2)}</pre>
+//   `;
+// }
+
+// async function estatisticas() {
+
+//   const response = await fetch(
+//     'http://localhost:3000/estatisticas'
+//   );
+
+//   const data = await response.json();
+
+//   document.getElementById('stats').textContent =
+//     JSON.stringify(data, null, 2);
+// }
