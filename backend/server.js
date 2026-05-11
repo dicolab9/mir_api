79bf481ed4da3e2e84c1ab1f1bb5e1c22a58f53b
dicolab9 <dico.colab9@gmail.com>
2026-05-11 13:59:22 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,15 @@ const app = express();
 
 app.disable('x-powered-by');
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://mir-api-6cip.onrender.com/'
+  ],
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
