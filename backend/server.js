3198c6ea601ab9069656719218539b31af19892f
dicolab9 <dico.colab9@gmail.com>
2026-05-11 13:56:01 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ require('./migrations');
 
 const app = express();
 
+app.disable('x-powered-by');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
